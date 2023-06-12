3ba6ffbe4ab6b9bab791bdf228c75bfa66cd70bf
Han Xu <keepsimple@gmail.com>
2023-06-11 22:23:48 -0700

    set red LED

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::thread::sleep;
+use std::time::Duration;
+use sysfs_gpio::{Direction, Pin};
+
 fn main() {
     println!("Hello, world!");
+    let red_led = Pin::new(12);
+    red_led.with_exported(|| {
+        red_led.set_direction(Direction::Out).unwrap();
+        red_led.set_value(1).unwrap();
+        sleep(Duration::from_millis(500));
+        red_led.set_value(0).unwrap();
+        Ok(())
+    }).unwrap();
 }
